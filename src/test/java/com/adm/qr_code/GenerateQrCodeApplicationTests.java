c05f393f65ca8d8c814f8057f4784d772ad7f940
Abdeladim ELBAKILI <abdeladim.2012@gmail.com>
2023-01-08 19:10:31 +0100

    Initial commit

---
+++ src/test/java/com/adm/qr_code/GenerateQrCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adm.qr_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateQrCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
